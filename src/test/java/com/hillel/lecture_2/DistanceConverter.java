7ad5ab31817baad169e10e8dc738fb1942470d6f
konstantin <k.litvinov@comparus.de>
2020-03-05 17:06:46 +0200

    refs 3 converter & 1 f

---
+++ src/test/java/com/hillel/lecture_2/DistanceConverter.java
@@ -0,0 +1,37 @@
+package com.hillel.lecture_2;
+
+import io.qameta.allure.Step;
+
+/**
+ * Created by alpa on 10/17/19
+ */
+public class DistanceConverter {
+
+    @Step
+    public double metersToInches(double meters) {
+//        TODO implements result
+        double result = (meters * 100) * 0.39;
+        return result;
+    }
+
+    @Step
+    public double inchesToMeters(double inches) {
+//        TODO implements result
+        double result = inches / 39.370;
+        return result;
+    }
+
+    @Step
+    public double milesToKilometres(double miles) {
+//        TODO implements result
+        double result = miles * 1.609;
+        return result;
+    }
+
+    @Step
+    public double kilometresToMiles(double kilometres) {
+//        TODO implements result
+        double result = kilometres / 1.609;
+        return result;
+    }
+}
